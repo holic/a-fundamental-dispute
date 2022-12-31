5e2f42d4255ec20220217c7c2d5aac89c11ff240
Kevin Ingersoll <kingersoll@gmail.com>
2022-12-30 23:22:45 -0800

    try only hover when supported

--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
     extend: {},
   },
