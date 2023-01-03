d2e995befb1efaccd45c53ae9b6f65605e4ba7d5
Kevin Ingersoll <kingersoll@gmail.com>
2023-01-02 22:01:55 -0800

    add opengraph image to gallery page

--- packages/app/src/pages/art.tsx
+++ packages/app/src/pages/art.tsx
@@ -21,6 +21,16 @@ const GalleryPage: NextPage = () => {
     <>
       <Head>
         <title>Gallery &mdash; A Fundamental Dispute</title>
+
+        <meta
+          property="og:image"
+          content={`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/thumbnail-2.jpg`}
+        />
+        <meta property="og:image:width" content="654" />
+        <meta property="og:image:height" content="900" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@generativelight" />
       </Head>
       <Gallery tokenIds={data?.tokens.map((token) => token.tokenId) ?? []} />
     </>
