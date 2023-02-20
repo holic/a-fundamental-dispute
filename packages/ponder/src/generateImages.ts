5bcbc7e7758b4464ff8f2887edc8aa404d488beb
Kevin Ingersoll <kingersoll@gmail.com>
2023-02-19 23:52:00 -0500

    Revert "uncomment image generation"
    This reverts commit 6e1069f380fe1db40234878e8e8795773073e24d.

--- packages/ponder/src/generateImages.ts
+++ packages/ponder/src/generateImages.ts
@@ -6,21 +6,16 @@ export const generateImages = async (
   seed: number,
   html: string
 ) => {
-  console.log("generating images", rendererAddress, tokenId, seed);
-  const res = await fetch("https://afd-renderer.up.railway.app/api/render", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      rendererAddress,
-      tokenId,
-      seed,
-      html,
-    }),
-  }).then((res) => res.json());
-  if (res.error) {
-    throw new Error(res.error);
-  }
-  console.log("image status", res.status, rendererAddress, tokenId, seed);
+  // console.log("generating images", rendererAddress, tokenId, seed);
+  // const res = await fetch("https://afd-renderer.up.railway.app/api/render", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({ rendererAddress, tokenId, seed, html }),
+  // }).then((res) => res.json());
+  // if (res.error) {
+  //   throw new Error(res.error);
+  // }
+  // console.log("image status", res.status, rendererAddress, tokenId, seed);
 };
