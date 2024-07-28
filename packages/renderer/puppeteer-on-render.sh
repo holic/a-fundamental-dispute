00a126df5dd273e1fe6bba12144fb2bba49b5bad
Kevin Ingersoll <kingersoll@gmail.com>
2024-07-28 18:40:52 +0100

    try new strategy
    from https://github.com/kwlockwo/puppeteer-native

---
+++ packages/renderer/puppeteer-on-render.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Store/pull Puppeteer cache with build cache
+if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then
+  echo "...Copying Puppeteer Cache from Build Cache"
+  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
+else
+  echo "...Storing Puppeteer Cache in Build Cache"
+  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
+fi
